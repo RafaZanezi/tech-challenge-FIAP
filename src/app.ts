8d8e034a1c2a3c10a62cfb5fd666c08049734623
Rafaela Zanezi <zar2ca@bosch.com>
2025-07-12 15:17:52 -0300

    configuração inicial do projeto

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+// import run from './config/dbConnect.js';
+// import routes from './routes/index.js';
+
+// run();
+const app = express();
+// routes(app);
+
+export default app;
